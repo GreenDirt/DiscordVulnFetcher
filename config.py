ebb58ac64b89b94bf30f8617bf34169023d8b3e2
GreenDirt <hugo@blx.ovh>
2023-04-17 23:16:00 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+VULDB_APIKEY = "TO_SET"
+DISCORD_BOT_TOKEN = "TO_SET"
+# Names of the product as in title section of threats
+PRODUCT_LIST = ["Windows", "Office 365", "Azure"]
+ERROR_CHANNEL_NAME = "errors"
