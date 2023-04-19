f84563f50cf54f946b586dd43de3948607943ad9
greendirt <hugo@blx.ovh>
2023-04-19 21:14:01 +0200

    Check

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-VULDB_APIKEY = "TO_SET"
-DISCORD_BOT_TOKEN = "TO_SET"
+VULDB_APIKEY = "4a8b772859130dd6f3a6d0e6f780a67c"
+DISCORD_BOT_TOKEN = "MTA5Njc5NTk2MDExOTY2ODg1Ng.GUJOpn.Bpombo1uwGzDsUAw6gjgMFaWIfuY58pP6gIF3Y"
 # Names of the product as in title section of threats
 PRODUCT_LIST = ["Windows", "Office 365", "Azure"]
 ERROR_CHANNEL_NAME = "errors"
